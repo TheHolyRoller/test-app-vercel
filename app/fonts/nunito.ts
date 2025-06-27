a1fa71abcb31d8d50722ec17cabecd390c856ab2
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2025-06-27 21:43:39 +0300

    bold

--- app/fonts/nunito.ts
+++ app/fonts/nunito.ts
@@ -2,7 +2,7 @@ import localFont from 'next/font/local'
 
 export const nunito = localFont({
   src: './Nunito.ttf',
-  weight: '400',
+  weight: '800',
   variable: '--font-nunito',
   display: 'swap',
 })
