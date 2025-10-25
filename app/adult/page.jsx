3ad4fb451bfce663cd45ac73fc057b2c6cbea4c7
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2025-10-25 19:53:31 +0300

    adult card added

---
+++ app/adult/page.jsx
@@ -0,0 +1,165 @@
+'use client'
+import React from 'react'
+
+import a from '../Styles/adult.module.css'; 
+import Image from 'next/image';
+// import affect from '../assets/Listen and learn, confident.svg'; 
+import affect from '../assets/Highlight Key Info.svg'; 
+import { useRouter } from 'next/navigation';
+import AdultCard from '../Components/AdultCard'; 
+
+
+function Adult() {
+
+
+
+  const router = useRouter();
+  
+  const handleNext = () => {
+    router.push('/function');
+  };
+
+
+
+  return (
+
+
+    <>
+
+    <AdultCard/>
+
+    {/* <section className={a.mainContainer} style={{backgroundColor: 'white'}}>
+    <div className={a.subContainer}>
+
+
+    <header className={a.headerImageContainer}>
+    <Image className={a.mainImage}  src={affect} width={5000} height={5000} alt='notes' /> 
+    </header>
+    
+
+    <div className={a.mainHeaderContainer}>
+        <h1 className={a.mainTextHeader}>
+
+        Adult Dyslexia Can Affect:
+        </h1>
+    </div>
+
+    <div>
+
+    
+    <article className={a.mainSupportingTextContainer}>
+
+        <ul className={a.affectsList}> 
+        <li className={a.affectsListItem}>
+
+            Reading 
+
+
+        </li>
+        <li className={a.affectsListItem}>
+
+                Writing  
+
+
+            </li> 
+            <li className={a.affectsListItem}>
+
+                Memory  
+
+                </li> 
+
+<li className={a.affectsListItem}>
+
+                Tests 
+
+
+            </li>
+         <li className={a.affectsListItem}>
+
+               Planning  
+
+
+            </li>
+
+
+        </ul>
+
+
+       </article>
+
+
+    <div className={a.ctaContainer}>
+
+
+  <div className={a.cta} onClick={handleNext}
+  
+  style={{cursor: 'pointer', fontSize: '2rem', letterSpacing: '1px'}}
+  >
+
+      Next 
+
+
+    </div>
+
+    
+    <div className={a.paginationElementContainer}
+      
+      style={{ margin: '0 auto'}}
+
+      >
+      <div className={a.paginationElementSubContainer}>
+
+        <ul className={a.paginationList}
+
+          style={{display: 'flex', gap: '0.8em'}}
+        
+        >
+
+          <li className={a.paginationItem} 
+          
+          style={{height: '5px', width: '33px', backgroundColor: 'rgb(227, 225, 228)', borderRadius: '14px', display: 'block'}}
+          
+          ></li>
+        <li className={a.paginationItem} 
+          
+          style={{height: '5px', width: '33px', backgroundColor: 'rgb(227, 225, 228)', borderRadius: '14px', display: 'block'}}
+          
+          ></li>
+             <li className={a.paginationItem} 
+          
+          style={{height: '5px', width: '33px', backgroundColor: 'rgb(153, 187, 154)', borderRadius: '14px', display: 'block'}}
+          
+          ></li>
+             <li className={a.paginationItem} 
+          
+          style={{height: '5px', width: '33px', backgroundColor: 'rgb(227, 225, 228)', borderRadius: '14px', display: 'block'}}
+          
+          ></li>
+            <li className={a.paginationItem} 
+          
+          style={{height: '5px', width: '33px', backgroundColor: 'rgb(227, 225, 228)', borderRadius: '14px', display: 'block'}}
+          
+          ></li>
+        </ul>
+      </div>
+      </div>
+
+
+    </div>
+
+
+    </div>
+
+    </div>
+
+
+    </section> */}
+
+
+    </>
+
+
+  )
+}
+
+export default Adult
